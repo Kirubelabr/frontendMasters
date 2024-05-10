4990c977c8307841034be9bfb5cff8177bf2c781
Kirubel A <kirubelabr@gmail.com>
2024-05-10 14:24:05 +0300

    Code splitting and server side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Suspense, lazy, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import AdoptedPetContext from "./AdoptedPetContext";
-import DetailsErrorBoundary from "./Details";
-import SearchParams from "./SearchParams";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,8 +24,14 @@ const App = () => {
         background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
       }}
     >
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center">
+              <h2 className="animate-spin text-5xl">🐶</h2>
+            </div>
+          }
+        >
           <AdoptedPetContext.Provider value={adoptedPet}>
             <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
               <Link className="text-6xl text-white hover:text-gray-200" to="/">
@@ -33,16 +39,14 @@ const App = () => {
               </Link>
             </header>
             <Routes>
-              <Route path="/details/:id" element={<DetailsErrorBoundary />} />
+              <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
           </AdoptedPetContext.Provider>
-        </QueryClientProvider>
-      </BrowserRouter>
+        </Suspense>
+      </QueryClientProvider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
